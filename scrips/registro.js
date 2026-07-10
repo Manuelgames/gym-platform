652a5cc1b239907b73902fa4273f08a8e166b877
Ma.garcia20 <ma.garcia20@info.uas.edu.mx>
2026-07-10 14:55:33 -0700

    avance en validaciones del apartado del registro

--- scrips/registro.js
+++ scrips/registro.js
@@ -4,23 +4,33 @@ const claveRegistro = document.getElementById('claveRegistro');
 const nacimientoRegistro = document.getElementById('nacimientoRegistro');
 const sexoRegistro = document.getElementById('sexoRegistro');
 const submitRegistro = document.getElementById('submitRegistro');
-
-
+const mensajeCorreoExistente = document.querySelector('.correo--existente');
+const mensajeCamposCompletados = document.querySelector('.campos--completados')
 
 
 submitRegistro.addEventListener('click', envioRegistro);
 
-
 function envioRegistro(eventoClick) {
-    console.log('entro');
-    console.log(nombreRegistro.value);
-    saveInLocalStorageForm(nombreRegistro.value,correoRegistro.value, claveRegistro.value, nacimientoRegistro.value, sexoRegistro.value)
     eventoClick.preventDefault();
-    nombreRegistro.value = '';
-    correoRegistro.value = '';
-    claveRegistro.value = '';
-    nacimientoRegistro.value = '';
-    sexoRegistro.value = '';
+    //existeCorreo: si es 0 quiere decir que el correo no esta registrado, si es 1 es que ya esta registrado
+    //camposCompletados: si es 0 es que los campos no se han completado
+
+    const validaciones = loadUsers();
+    console.log(validaciones)
+    if (validaciones.camposCompletados == 1) {
+        if (validaciones.existeCorreo == 0) {
+            mensajeCorreoExistente.style.display = 'none';
+            saveInLocalStorageForm(nombreRegistro.value, correoRegistro.value, claveRegistro.value, nacimientoRegistro.value, sexoRegistro.value)
+            console.log('correo registrado')
+            nombreRegistro.value = '';
+            correoRegistro.value = '';
+            claveRegistro.value = '';
+            nacimientoRegistro.value = '';
+            sexoRegistro.value = '';
+        }
+    }
+    validaciones.camposCompletados = 0;
+    validaciones.existeCorreo = 0
 }
 
 
@@ -44,4 +54,37 @@ function saveInLocalStorageForm(nombre, correo, clave, nacimiento, sexo) {
     localStorage.setItem('sexoRegistro', JSON.stringify(sexoRegistro));
 }
 
+function loadUsers() {
+    console.log('loadusers')
+    let validaciones = {
+        //si es 0 quiere decir que el correo no esta registrado, si es 1 es que ya esta registrado
+        existeCorreo: 0,
+        //si es 0 es que los campos no se han completado
+        camposCompletados: 0
+    }
 
+    const correoRegistroExistente = JSON.parse(localStorage.getItem('correoRegistro') || '[]');
+
+    if (nombreRegistro.value != '' && correoRegistro.value != '' && claveRegistro.value != '' && nacimientoRegistro.value != '' && sexoRegistro.value != '') {
+        //se completo el llenado de todos los campos
+        validaciones.camposCompletados = 1;
+        mensajeCamposCompletados.style.display = 'none'
+        //validacion si en caso de que ya exista el correo
+        const correoExistente = correoRegistroExistente.filter(function (correos) {
+            return correos === correoRegistro.value;
+        })
+        for (correo of correoExistente) {
+            if (correo) {
+                mensajeCorreoExistente.style.display = 'block'
+                validaciones.existeCorreo = 1;
+                console.log('Este correo ya esta registrado')
+                return validaciones;
+            }
+        }
+    }
+    else {
+        console.log('campos no completados');
+        mensajeCamposCompletados.style.display = 'block'
+    }
+    return validaciones;
+}
